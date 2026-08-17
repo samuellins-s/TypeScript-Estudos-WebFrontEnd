2ee9d3442583d5edf6bc476b244defb021a9a92a
samuellins <rnsamueldlins@gmail.com>
2026-08-17 20:12:07 -0300

    Create 01.ts

---
+++ 01.ts
@@ -0,0 +1,8 @@
+// O typescript é de Tipagem Estática: o tipo da variável é explicitamente declarado e não pode ser alterado durante o código
+
+let age: number;
+
+age = 21;
+typeof age // number
+
+// age = '21' // error: age must be of type number
