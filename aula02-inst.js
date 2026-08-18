13bc45a7ae356fdf789f49ef1f667eaae3d7b31f
samuellins <rnsamueldlins@gmail.com>
2026-08-17 21:59:57 -0300

    aula02 ts e js

---
+++ aula02-inst.js
@@ -0,0 +1,6 @@
+"use strict";
+let nome = 'Bruno';
+function ola(nm) {
+    return `Seja bem-vindo, ${nm}!`;
+}
+console.log(ola(nome));
---
+++ aula02-inst.ts
@@ -0,0 +1,5 @@
+let nome: string = 'Bruno'
+function ola(nm: string): string {
+return `Seja bem-vindo, ${nm}!`
+}
+console.log(ola(nome))
