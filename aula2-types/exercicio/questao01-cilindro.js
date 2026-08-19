1133aa1927addc5b899c7e24b5540cf70fbcb022
samuellins <rnsamueldlins@gmail.com>
2026-08-19 19:35:02 -0300

    questao01

---
+++ aula2-types/exercicio/questao01-cilindro.js
@@ -0,0 +1,5 @@
+"use strict";
+let altura = 2;
+let raio = 3;
+let volume = Math.PI * (raio ** 2) * altura;
+console.log(`O volume do cilindo é: ${volume}`);
---
+++ aula2-types/exercicio/questao01-cilindro.ts
@@ -0,0 +1,5 @@
+let altura: number = 2
+let raio: number = 3
+let volume: number = Math.PI * (raio**2) * altura
+
+console.log(`O volume do cilindo é: ${volume}`)
