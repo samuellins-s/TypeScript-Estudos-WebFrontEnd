ea9f2e6b3c5812c0b844801beac2ed6e1fa36d28
samuellins <rnsamueldlins@gmail.com>
2026-08-19 19:35:27 -0300

    questao04

---
+++ aula2-types/exercicio/questao04-jsonformat.ts
@@ -0,0 +1,8 @@
+let aluno = {
+    "nome": "Samuel",
+    "matricula": 20261148060002,
+    "curso": "Tecnologia em Sistemas para Internet",
+    "IRA": 79
+}
+
+console.log(`${aluno.nome} é aluno de ${aluno.curso} com matrícula ${aluno.matricula} e possui IRA ${aluno.IRA}.`)
