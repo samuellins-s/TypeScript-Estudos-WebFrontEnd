727399be71d92ec1e13c98a40bdf29566b44558c
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-08-25 08:03:00 -0300

    Update json.ts

--- aula2-types/json.ts
+++ aula2-types/json.ts
@@ -1,3 +1,5 @@
+// JSON -> Java Script Object Notation
+
 let carro = { // valor: atributo
     'nome': 'Uno',
     'marca': 'Fiat',
@@ -5,3 +7,22 @@ let carro = { // valor: atributo
 }
 
 console.log(carro.marca) // Fiat
+
+enum Curso { // enum: conjunto de constantes
+    TSI = 'Sistemas para Internet',
+    INFO = 'Informática integrado'
+}
+
+type Estudante = {
+    nome: string
+    matricula: number
+    curso: Curso
+    ira: number
+}
+
+const samuel: Estudante = {
+    'nome': 'Samuel Lins',
+    'matricula': 2026118060002,
+    'curso': Curso.TSI,
+    'ira': 80
+}
