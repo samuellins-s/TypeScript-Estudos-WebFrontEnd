7a85223a8bb7883c697355739e8c29a71700d551
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-09-01 08:21:01 -0300

    Create questao01.ts

---
+++ aula3-for/exercicio3/questao01.ts
@@ -0,0 +1,9 @@
+// cada familia
+let qtdFamilias: number = 3
+let rendaFamiliar: number = 1000
+
+// todas familias
+let todasRendaFamiliar: number = qtdFamilias * rendaFamiliar
+let mediaFilhosFamilia: number = 3
+let media: number = rendaFamiliar / mediaFilhosFamilia
+
