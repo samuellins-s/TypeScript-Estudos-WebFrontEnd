31b7c3505586682d07f9f301b986263cbdfe03a7
samuellins <rnsamueldlins@gmail.com>
2026-09-04 16:07:36 -0300

    Create questao01.ts

---
+++ aula4 - laco/exercicio4/questao01.ts
@@ -0,0 +1,21 @@
+/*
+
+calculo da renda familiar da cidade -> todas familias e faço a media: total dinheiro / quantidade familia
+
+calculo do numero de filhos de cada familia -> todas familias e faço a media -> total de filhos existentes / quantidade de familias
+
+*/
+
+let soma_renda: number = 600000
+let soma_filhos: number = 300
+let quantidade_familias: number = 16
+let media_renda: number = 0
+
+while (soma_renda >= 0 && soma_filhos >= 0) {
+    let media_renda: number = soma_renda / quantidade_familias
+    let media_filhos: number = soma_filhos / quantidade_familias
+    console.log(`Valor da media da renda: ${media_renda}`)
+    
+}
+
+
