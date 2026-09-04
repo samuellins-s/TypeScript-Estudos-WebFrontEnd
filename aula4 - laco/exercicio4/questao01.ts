c4413379f2aeadc862f56c019bbd18c6bb05c0a0
samuellins <rnsamueldlins@gmail.com>
2026-09-04 17:06:54 -0300

    Update questao01.ts

--- aula4 - laco/exercicio4/questao01.ts
+++ aula4 - laco/exercicio4/questao01.ts
@@ -9,13 +9,13 @@ calculo do numero de filhos de cada familia -> todas familias e faço a media ->
 let soma_renda: number = 600000
 let soma_filhos: number = 300
 let quantidade_familias: number = 16
-let media_renda: number = 0
 
 while (soma_renda >= 0 && soma_filhos >= 0) {
     let media_renda: number = soma_renda / quantidade_familias
     let media_filhos: number = soma_filhos / quantidade_familias
     console.log(`Valor da media da renda: ${media_renda}`)
-    
+    console.log(`Valor da media dos filhos: ${media_filhos}`)
+
 }
 
 
