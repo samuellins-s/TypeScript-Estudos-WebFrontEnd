cc4f445f6b426fbfbbfe92b5759a0d74202c4705
samuellins <rnsamueldlins@gmail.com>
2026-09-05 12:51:53 -0300

    Delete questao01.ts

--- aula3-for/exercicio3/questao01.ts
+++
@@ -1,9 +0,0 @@
-// cada familia
-let qtdFamilias: number = 3
-let rendaFamiliar: number = 1000
-
-// todas familias
-let todasRendaFamiliar: number = qtdFamilias * rendaFamiliar
-let mediaFilhosFamilia: number = 3
-let media: number = rendaFamiliar / mediaFilhosFamilia
-
